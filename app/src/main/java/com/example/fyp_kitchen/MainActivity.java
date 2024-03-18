9178a970b8929d4560e87a2cb6dcf2ae392e1794
ahmedtariqkhan <ahmedkhansaduzai@gmail.com>
2024-03-18 18:24:19 +0500

    Removed Permission

--- app/src/main/java/com/example/fyp_kitchen/MainActivity.java
+++ app/src/main/java/com/example/fyp_kitchen/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fyp_kitchen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
     public void onGotoImageActivity(View view) {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new string[]{Manifest.permission.READ_EXTERNAL_STORAGE}.requestcode 0);
+//            requestPermissions(new string[]{Manifest.permission.READ_EXTERNAL_STORAGE}.requestcode 0);
         }
-        int a = 0;
+        int a = 3;
     }
 }
